b3bd71af7ff12013097e2390732a5c5eb902788d
smuldev <smultypefake@gmail.com>
2024-07-01 13:53:31 -0300

    Termino do site. Adição de moedas, alerta e correção nas conversões.

--- scripts.js
+++ scripts.js
@@ -2,15 +2,32 @@ const input = document.querySelector(".valor")
 const currencySelect = document.querySelector("#currency-select")
 const currencySelectTo = document.querySelector(".currency-selectTo")
 const currencySelectFrom = document.querySelector(".currency-selectFrom")
+const alert = document.querySelector("#alert")
+
 
 // Bandeiras //
 const flagUp = document.querySelector(".flag1")
 const flagDown = document.querySelector(".flag2")
 
-// Valor Moedas //
+// Valor Moedas em relacao ao real //
 const dolarToday = 5.2
 const euroToday = 6.2
-const realToday = 0.18
+const btcToday = 0.0000028
+
+// Valor moedas em relacao ao euro //
+const dolarTodayEu = 1.07140
+const realTodayEu = 5.98
+const btcTodayEu = 0.000017
+
+// Valor moedas em relacao ao dolar //
+const realTodayUs = 5.58
+const btcTodayUs = 0.000016
+const euroTodayUs = 0.93
+
+// Valor moedas em relacao ao BTC //
+const realTodayBtc = 351750.66
+const dolarTodayBtc = 62930
+const euroTodayBtc = 58678.61
 
 
 // Mudar bandeiras //
@@ -32,6 +49,10 @@ function changeFlag() {
     imgUp.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/2000px-Flag_of_Brazil.svg.png'
   }
 
+  if (currencySelectFrom.value == "bitcoin") {
+    imgUp.src = './img/Btc.png'
+  }
+
 
   // Bandeira de baixo //
   if (currencySelectTo.value == "euro") { // Se o estiver selecionado o euro, a imagem de baixo ira mudar //
@@ -46,6 +67,10 @@ function changeFlag() {
     imgDown.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/2000px-Flag_of_Brazil.svg.png'
   }
 
+  if (currencySelectTo.value == "bitcoin") {
+    imgDown.src = './img/Btc.png'
+  }
+
   keypress()
 }
 
@@ -54,24 +79,125 @@ function changeFlag() {
 // Conversão //
 function keypress() {
   const inputValue = document.querySelector(".valor").value
+  
+    // Trocar placeholder //
+    if (currencySelectFrom.value == "euro") {
+      input.placeholder = '€ 10.000'
+    }
+  
+    if (currencySelectFrom.value == "real") {
+      input.placeholder = 'R$ 10.000'
+    }
+  
+    if (currencySelectFrom.value == "dolar") {
+      input.placeholder = 'U$ 10.000'
+    }
+  
+    if (currencySelectFrom.value == "bitcoin") {
+      input.placeholder = 'BTC 10.000'
+    }  
 
-  if (currencySelect.value == "dolar") { // Se o select estiver selecionado o valor de dolar, execute aqui! //
-    document.querySelector(".convertedValue").value = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD"
-    }).format(inputValue / dolarToday)
-  }
-
-  if (currencySelect.value == "euro") { // Se o select estiver selecionado o valor de euro, execute aqui! //
+  // Euro //
+  if (currencySelectFrom.value == "euro" && currencySelectTo.value == "euro") { // Se o select estiver selecionado o valor de dolar, execute aqui! //
     document.querySelector(".convertedValue").value = new Intl.NumberFormat("pt-PT", {
       style: "currency",
       currency: "EUR"
-    }).format(inputValue / euroToday)
-  }
+    }).format(inputValue)
+  } else if (currencySelectFrom.value == "euro" && currencySelectTo.value == "dolar") {
+    document.querySelector(".convertedValue").value = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD"
+    }).format(inputValue * dolarTodayEu)
+  } else if (currencySelectFrom.value == "euro" && currencySelectTo.value == "real") {
+    document.querySelector(".convertedValue").value = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    }).format(inputValue * realTodayEu)
+  } else if (currencySelectFrom.value == "euro" && currencySelectTo.value == "bitcoin") {
+    document.querySelector(".convertedValue").value = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "BTC",
+      maximumFractionDigits: 8
+    }).format(inputValue * btcTodayEu)
+  } 
 
-  if (currencySelect.value == "empty") {
-    alert("Selecione as moedas!")
-  }
+  // Dolar //
+  if (currencySelectFrom.value == "dolar" && currencySelectTo.value == "dolar") { // Se o select estiver selecionado o valor de dolar, execute aqui! //
+    document.querySelector(".convertedValue").value = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD"
+    }).format(inputValue)
+  } else if (currencySelectFrom.value == "dolar" && currencySelectTo.value == "euro") {
+    document.querySelector(".convertedValue").value = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR"
+    }).format(inputValue * euroTodayUs)
+  } else if (currencySelectFrom.value == "dolar" && currencySelectTo.value == "real") {
+    document.querySelector(".convertedValue").value = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    }).format(inputValue * realTodayUs)
+  } else if (currencySelectFrom.value == "dolar" && currencySelectTo.value == "bitcoin") {
+    document.querySelector(".convertedValue").value = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "BTC",
+      maximumFractionDigits: 8
+    }).format(inputValue * btcTodayUs)
+  } 
+
+  // BitCoin //
+  if (currencySelectFrom.value == "bitcoin" && currencySelectTo.value == "bitcoin") { // Se o select estiver selecionado o valor de dolar, execute aqui! //
+    document.querySelector(".convertedValue").value = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "BTC",
+      maximumFractionDigits: 8
+    }).format(inputValue)
+  } else if (currencySelectFrom.value == "bitcoin" && currencySelectTo.value == "euro") {
+    document.querySelector(".convertedValue").value = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR"
+    }).format(inputValue * euroTodayBtc)
+  } else if (currencySelectFrom.value == "bitcoin" && currencySelectTo.value == "real") {
+    document.querySelector(".convertedValue").value = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    }).format(inputValue * realTodayBtc)
+  } else if (currencySelectFrom.value == "bitcoin" && currencySelectTo.value == "dolar") {
+    document.querySelector(".convertedValue").value = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD"
+    }).format(inputValue * dolarTodayBtc)
+  } 
+
+// Real //
+if (currencySelectFrom.value == "real" && currencySelectTo.value == "real") { // Se o select estiver selecionado o valor de dolar, execute aqui! //
+  document.querySelector(".convertedValue").value = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  }).format(inputValue)
+} else if (currencySelectFrom.value == "real" && currencySelectTo.value == "euro") {
+  document.querySelector(".convertedValue").value = new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR"
+  }).format(inputValue / euroToday)
+} else if (currencySelectFrom.value == "real" && currencySelectTo.value == "bitcoin") {
+  document.querySelector(".convertedValue").value = new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "BTC",
+    maximumFractionDigits: 8
+  }).format(inputValue * btcToday)
+} else if (currencySelectFrom.value == "real" && currencySelectTo.value == "dolar") {
+  document.querySelector(".convertedValue").value = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(inputValue / dolarToday)
+}
+
+ if (currencySelectTo.value == "empty") {
+  alert.style.display = 'block'
+} else { ;
+  alert.style.display = "none"
+}
 
 }
 
